2e1aeb1c733220bc2004031301c6ddbd8e88afe9
hoangnym <hoangnym94@gmail.com>
2021-07-18 17:34:59 +0200

    start guessing the number proeoject under name server.py

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 def make_bold(function):
     def wrapper():
         return f"<b>{function()}</b>"
     return wrapper
+
 
 def make_emphasis(function):
     def wrapper():
         return f"<em>{function()}</em>"
     return wrapper
 
+
 def make_underlined(function):
     def wrapper():
         return f"<u>{function()}</u>"
     return wrapper
+
 
 @app.route('/')
 def hello_world():
@@ -24,12 +28,14 @@ def hello_world():
            '<img width="400px"' \
            'src="https://media.4-paws.org/1/e/d/6/1ed6da75afe37d82757142dc7c6633a532f53a7d/VIER%20PFOTEN_2019-03-15_001-2886x1999-1920x1330.jpg">'
 
+
 @app.route('/bye')
 @make_bold
 @make_emphasis
 @make_underlined
 def bye():
     return 'Bye!'
+
 
 # use <name> to input variable in url
 @app.route("/username/<name>/<int:number>")
